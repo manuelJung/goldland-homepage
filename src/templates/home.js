dcb4b22237a6d2bc1927f13ffb9a6431ea7b84f5
manuelJung <manu.jung@relaxcommerce.com>
2019-06-09 19:14:21 +0200

    updated home route

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Image from "../components/image"
+import { ParallaxProvider } from 'react-scroll-parallax'
 
 export default function HomeRoute ({pageContext}) {
   return (
-    <Layout>
-      <h1>HOME ({pageContext.lang})</h1>
-    </Layout>
+    <ParallaxProvider>
+      <Layout>
+        <div style={{height: '100vh'}}>
+          <Image />
+        </div>
+        <h1>HOME ({pageContext.lang})</h1>
+      </Layout>
+    </ParallaxProvider>
   )
 }
